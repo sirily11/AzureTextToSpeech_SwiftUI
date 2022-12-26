a10b77ab4d5fd9b1f2256731ecd86588a4b92a71
sirily11 <sirily1997@gmail.com>
2022-12-26 11:08:55 +0800

    Initial Commit

---
+++ TextToSpeech/TextToSpeechApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextToSpeechApp.swift
+//  TextToSpeech
+//
+//  Created by Qiwei Li on 12/26/22.
+//
+
+import SwiftUI
+
+@main
+struct TextToSpeechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
